0b7a3ab897160b87905520d2e1adc6995f801c86
German Saprykin <saprykin.h@gmail.com>
2018-06-29 22:10:43 +0800

    Format.

--- lib/src/server/interceptor.dart
+++ lib/src/server/interceptor.dart
@@ -4,9 +4,10 @@ import 'call.dart';
 import 'service.dart';
 
 /// A gRPC Interceptor.
-/// 
+///
 /// Interceptor is called before correspoding [ServiceMethod] invocation.
 /// If interceptor returns [GrpcError], the error will be returned as a response and [ServiceMethod] wouldn't be called.
 /// If interceptor throws [Exception], [GrpcError.internal] with exception.toString() will be returned.
 /// If interceptor returns null, correspoding [ServiceMethod] of [Service] will be called.
-typedef Interceptor = GrpcError Function(ServiceCall call, ServiceMethod method);
+typedef Interceptor = GrpcError Function(
+    ServiceCall call, ServiceMethod method);
