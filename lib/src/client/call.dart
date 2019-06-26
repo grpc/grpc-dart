a5956f26cd24f367b35befb60efe03e631974f40
Taehyun Park <root@njovy.com>
2019-06-26 16:23:48 +0900

    Get rid of a commented line. Keep values from header upon parsing.

--- lib/src/client/call.dart
+++ lib/src/client/call.dart
@@ -240,13 +240,11 @@ class ClientCall<Q, R> implements Response {
       // Only received a header frame and no data frames, so the header
       // should contain "trailers" as well (Trailers-Only).
       _trailers.complete(_headerMetadata);
-      _headerMetadata.remove(":status");
-      final status = _headerMetadata.remove('grpc-status');
-//      final status = _headerMetadata['grpc-status'];
+      final status = _headerMetadata['grpc-status'];
       // If status code is missing, we must treat it as '0'. As in 'success'.
       final statusCode = status != null ? int.parse(status) : 0;
       if (statusCode != 0) {
-        final message = _headerMetadata.remove('grpc-message');
+        final message = _headerMetadata['grpc-message'];
         _responseError(
             new GrpcError.custom(statusCode, message, _headerMetadata));
       }
