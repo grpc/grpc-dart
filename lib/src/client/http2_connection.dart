39e0d3b9042794fa40b58e6aea8caa2fc9be4592
Simon Lightfoot <simon@devangels.london>
2019-11-22 11:38:12 +0000

    Fixes bug where connect state is mismanaged on socket error.

--- lib/src/client/http2_connection.dart
+++ lib/src/client/http2_connection.dart
@@ -90,7 +90,8 @@ class Http2ClientConnection implements connection.ClientConnection {
           onBadCertificate: _validateBadCertificate);
     }
 
-    final connection = ClientTransportConnection.viaSocket(socket);
+    final incoming = socket.handleError((_) => _abandonConnection());
+    final connection = ClientTransportConnection.viaStreams(incoming, socket);
     socket.done.then((_) => _abandonConnection());
 
     // Give the settings settings-frame a bit of time to arrive.
@@ -265,7 +266,7 @@ class Http2ClientConnection implements connection.ClientConnection {
     _cancelTimer();
     _transportConnection = null;
 
-    if (_state == ConnectionState.idle && _state == ConnectionState.shutdown) {
+    if (_state == ConnectionState.idle || _state == ConnectionState.shutdown) {
       // All good.
       return;
     }
