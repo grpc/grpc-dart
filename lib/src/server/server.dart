c8212928e42f8f67d0e145120236dff56340c56c
Igor Demyanov <igor.demyanov@gmail.com>
2019-11-29 10:31:02 +0300

    fix code format

--- lib/src/server/server.dart
+++ lib/src/server/server.dart
@@ -100,7 +100,7 @@ class Server {
           port ?? 443,
           security.securityContext,
           backlog: backlog,
-          shared: shared ,
+          shared: shared,
           v6Only: v6Only);
       server = _secureServer;
     } else {
