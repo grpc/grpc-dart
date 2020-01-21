61c88cc117911ff583a708adc0c82da7a12adf64
Stryder <stryder@strangedreamsinc.com>
2020-01-21 18:00:23 -0500

    Re-generate pb.dart files for this example.

--- example/helloworld/lib/src/generated/helloworld.pbgrpc.dart
+++ example/helloworld/lib/src/generated/helloworld.pbgrpc.dart
@@ -1,12 +1,13 @@
 ///
 //  Generated code. Do not modify.
 //  source: helloworld.proto
-///
-// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
 
 import 'dart:async' as $async;
 
-import 'dart:core' as $core show int, String, List;
+import 'dart:core' as $core;
 
 import 'package:grpc/service_api.dart' as $grpc;
 import 'helloworld.pb.dart' as $0;
@@ -17,6 +18,11 @@ class GreeterClient extends $grpc.Client {
       '/helloworld.Greeter/SayHello',
       ($0.HelloRequest value) => value.writeToBuffer(),
       ($core.List<$core.int> value) => $0.HelloReply.fromBuffer(value));
+  static final _$sayHelloAgain =
+      $grpc.ClientMethod<$0.HelloRequest, $0.HelloReply>(
+          '/helloworld.Greeter/SayHelloAgain',
+          ($0.HelloRequest value) => value.writeToBuffer(),
+          ($core.List<$core.int> value) => $0.HelloReply.fromBuffer(value));
 
   GreeterClient($grpc.ClientChannel channel, {$grpc.CallOptions options})
       : super(channel, options: options);
@@ -24,6 +30,14 @@ class GreeterClient extends $grpc.Client {
   $grpc.ResponseFuture<$0.HelloReply> sayHello($0.HelloRequest request,
       {$grpc.CallOptions options}) {
     final call = $createCall(_$sayHello, $async.Stream.fromIterable([request]),
+        options: options);
+    return $grpc.ResponseFuture(call);
+  }
+
+  $grpc.ResponseFuture<$0.HelloReply> sayHelloAgain($0.HelloRequest request,
+      {$grpc.CallOptions options}) {
+    final call = $createCall(
+        _$sayHelloAgain, $async.Stream.fromIterable([request]),
         options: options);
     return $grpc.ResponseFuture(call);
   }
@@ -40,13 +54,27 @@ abstract class GreeterServiceBase extends $grpc.Service {
         false,
         ($core.List<$core.int> value) => $0.HelloRequest.fromBuffer(value),
         ($0.HelloReply value) => value.writeToBuffer()));
+    $addMethod($grpc.ServiceMethod<$0.HelloRequest, $0.HelloReply>(
+        'SayHelloAgain',
+        sayHelloAgain_Pre,
+        false,
+        false,
+        ($core.List<$core.int> value) => $0.HelloRequest.fromBuffer(value),
+        ($0.HelloReply value) => value.writeToBuffer()));
   }
 
   $async.Future<$0.HelloReply> sayHello_Pre(
-      $grpc.ServiceCall call, $async.Future request) async {
+      $grpc.ServiceCall call, $async.Future<$0.HelloRequest> request) async {
     return sayHello(call, await request);
   }
 
+  $async.Future<$0.HelloReply> sayHelloAgain_Pre(
+      $grpc.ServiceCall call, $async.Future<$0.HelloRequest> request) async {
+    return sayHelloAgain(call, await request);
+  }
+
   $async.Future<$0.HelloReply> sayHello(
+      $grpc.ServiceCall call, $0.HelloRequest request);
+  $async.Future<$0.HelloReply> sayHelloAgain(
       $grpc.ServiceCall call, $0.HelloRequest request);
 }
