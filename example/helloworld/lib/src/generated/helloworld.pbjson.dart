148a8ca44f25915b8d018d3c34ace3bcf049de30
Patrice Chalin <pchalin@gmail.com>
2020-02-27 17:36:15 -0500

    helloworld and route_guide: update to protobuf v1

--- example/helloworld/lib/src/generated/helloworld.pbjson.dart
+++ example/helloworld/lib/src/generated/helloworld.pbjson.dart
@@ -1,19 +1,21 @@
 ///
 //  Generated code. Do not modify.
 //  source: helloworld.proto
-///
-// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
 
-const HelloRequest$json = {
+const HelloRequest$json = const {
   '1': 'HelloRequest',
-  '2': [
-    {'1': 'name', '3': 1, '4': 1, '5': 9, '10': 'name'},
+  '2': const [
+    const {'1': 'name', '3': 1, '4': 1, '5': 9, '10': 'name'},
   ],
 };
 
-const HelloReply$json = {
+const HelloReply$json = const {
   '1': 'HelloReply',
-  '2': [
-    {'1': 'message', '3': 1, '4': 1, '5': 9, '10': 'message'},
+  '2': const [
+    const {'1': 'message', '3': 1, '4': 1, '5': 9, '10': 'message'},
   ],
 };
+
--- example/route_guide/lib/src/generated/route_guide.pbjson.dart
+++ example/route_guide/lib/src/generated/route_guide.pbjson.dart
@@ -1,61 +1,49 @@
 ///
 //  Generated code. Do not modify.
 //  source: route_guide.proto
-///
-// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
 
-const Point$json = {
+const Point$json = const {
   '1': 'Point',
-  '2': [
-    {'1': 'latitude', '3': 1, '4': 1, '5': 5, '10': 'latitude'},
-    {'1': 'longitude', '3': 2, '4': 1, '5': 5, '10': 'longitude'},
+  '2': const [
+    const {'1': 'latitude', '3': 1, '4': 1, '5': 5, '10': 'latitude'},
+    const {'1': 'longitude', '3': 2, '4': 1, '5': 5, '10': 'longitude'},
   ],
 };
 
-const Rectangle$json = {
+const Rectangle$json = const {
   '1': 'Rectangle',
-  '2': [
-    {'1': 'lo', '3': 1, '4': 1, '5': 11, '6': '.routeguide.Point', '10': 'lo'},
-    {'1': 'hi', '3': 2, '4': 1, '5': 11, '6': '.routeguide.Point', '10': 'hi'},
+  '2': const [
+    const {'1': 'lo', '3': 1, '4': 1, '5': 11, '6': '.routeguide.Point', '10': 'lo'},
+    const {'1': 'hi', '3': 2, '4': 1, '5': 11, '6': '.routeguide.Point', '10': 'hi'},
   ],
 };
 
-const Feature$json = {
+const Feature$json = const {
   '1': 'Feature',
-  '2': [
-    {'1': 'name', '3': 1, '4': 1, '5': 9, '10': 'name'},
-    {
-      '1': 'location',
-      '3': 2,
-      '4': 1,
-      '5': 11,
-      '6': '.routeguide.Point',
-      '10': 'location'
-    },
+  '2': const [
+    const {'1': 'name', '3': 1, '4': 1, '5': 9, '10': 'name'},
+    const {'1': 'location', '3': 2, '4': 1, '5': 11, '6': '.routeguide.Point', '10': 'location'},
   ],
 };
 
-const RouteNote$json = {
+const RouteNote$json = const {
   '1': 'RouteNote',
-  '2': [
-    {
-      '1': 'location',
-      '3': 1,
-      '4': 1,
-      '5': 11,
-      '6': '.routeguide.Point',
-      '10': 'location'
-    },
-    {'1': 'message', '3': 2, '4': 1, '5': 9, '10': 'message'},
+  '2': const [
+    const {'1': 'location', '3': 1, '4': 1, '5': 11, '6': '.routeguide.Point', '10': 'location'},
+    const {'1': 'message', '3': 2, '4': 1, '5': 9, '10': 'message'},
   ],
 };
 
-const RouteSummary$json = {
+const RouteSummary$json = const {
   '1': 'RouteSummary',
-  '2': [
-    {'1': 'point_count', '3': 1, '4': 1, '5': 5, '10': 'pointCount'},
-    {'1': 'feature_count', '3': 2, '4': 1, '5': 5, '10': 'featureCount'},
-    {'1': 'distance', '3': 3, '4': 1, '5': 5, '10': 'distance'},
-    {'1': 'elapsed_time', '3': 4, '4': 1, '5': 5, '10': 'elapsedTime'},
+  '2': const [
+    const {'1': 'point_count', '3': 1, '4': 1, '5': 5, '10': 'pointCount'},
+    const {'1': 'feature_count', '3': 2, '4': 1, '5': 5, '10': 'featureCount'},
+    const {'1': 'distance', '3': 3, '4': 1, '5': 5, '10': 'distance'},
+    const {'1': 'elapsed_time', '3': 4, '4': 1, '5': 5, '10': 'elapsedTime'},
   ],
 };
+
