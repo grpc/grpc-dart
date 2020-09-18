aaf61865d25e6b8ef5fcbe42f9a59366500f5adf
Vyacheslav Egorov <vegorov@google.com>
2020-09-18 15:24:16 +0200

    Add simple end-to-end gRPC-web test
    We did not have any before which allowed for regressions like #306 to
    slip through. Unfortunately we can't test gRPC-web implementation
    in pure Dart because we don't have a server side implementation of
    the protocol. Instead we add a dependency on the third party
    gRPC-web proxy (grpcwebproxy by Improbable - the choice made
    purely based on the simplicity of installation) which forwards all
    request gRPC server (written in Dart).

---
+++ test/src/generated/echo.pbjson.dart
@@ -0,0 +1,37 @@
+///
+//  Generated code. Do not modify.
+//  source: echo.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+const EchoRequest$json = const {
+  '1': 'EchoRequest',
+  '2': const [
+    const {'1': 'message', '3': 1, '4': 1, '5': 9, '10': 'message'},
+  ],
+};
+
+const EchoResponse$json = const {
+  '1': 'EchoResponse',
+  '2': const [
+    const {'1': 'message', '3': 1, '4': 1, '5': 9, '10': 'message'},
+  ],
+};
+
+const ServerStreamingEchoRequest$json = const {
+  '1': 'ServerStreamingEchoRequest',
+  '2': const [
+    const {'1': 'message', '3': 1, '4': 1, '5': 9, '10': 'message'},
+    const {'1': 'message_count', '3': 2, '4': 1, '5': 5, '10': 'messageCount'},
+    const {'1': 'message_interval', '3': 3, '4': 1, '5': 5, '10': 'messageInterval'},
+  ],
+};
+
+const ServerStreamingEchoResponse$json = const {
+  '1': 'ServerStreamingEchoResponse',
+  '2': const [
+    const {'1': 'message', '3': 1, '4': 1, '5': 9, '10': 'message'},
+  ],
+};
+
