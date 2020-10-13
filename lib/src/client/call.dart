ee36623ad2b8df9dff3754bb8a1bd7d44cfffb22
Nic Hite <nichite@google.com>
2020-10-13 01:49:26 -0700

    fix typo.

--- lib/src/client/call.dart
+++ lib/src/client/call.dart
@@ -224,7 +224,7 @@ class ClientCall<Q, R> implements Response {
   void _sendRequest(ClientConnection connection, Map<String, String> metadata) {
     try {
       _stream = connection.makeRequest(
-          _method.path, options.timeout, metxadata, _onRequestError,
+          _method.path, options.timeout, metadata, _onRequestError,
           callOptions: options);
     } catch (e) {
       _terminateWithError(GrpcError.unavailable('Error making call: $e'));
