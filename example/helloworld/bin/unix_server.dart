52bea07000d38878604ebeb1804d6cb2eee55e8c
Lei Liu <64953129+llapx@users.noreply.github.com>
2020-11-11 22:43:05 +0800

    Add support for unix domain sockets (#327)
    Fixes #299

---
+++ example/helloworld/bin/unix_server.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2020, the gRPC project authors. Please see the AUTHORS file
+// for details. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// Dart implementation of the gRPC helloworld.Greeter server.
+import 'dart:io';
+import 'package:grpc/grpc.dart';
+
+import 'package:helloworld/src/generated/helloworld.pb.dart';
+import 'package:helloworld/src/generated/helloworld.pbgrpc.dart';
+
+class GreeterService extends GreeterServiceBase {
+  @override
+  Future<HelloReply> sayHello(ServiceCall call, HelloRequest request) async {
+    return HelloReply()..message = 'Hello, ${request.name}!';
+  }
+}
+
+Future<void> main(List<String> args) async {
+  final udsAddress =
+      InternetAddress('localhost', type: InternetAddressType.unix);
+  final server = Server([GreeterService()]);
+  await server.serve(address: udsAddress);
+  print('Start UNIX Server @localhost...');
+}
