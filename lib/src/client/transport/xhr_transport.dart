0939fc1743461e1c1f65bd2e3ceaae51c46eb0c2
Alex Herrera <alex.herrera@joylabs.com>
2020-12-18 15:55:20 -0800

    Replace fix with feedback fix

--- lib/src/client/transport/xhr_transport.dart
+++ lib/src/client/transport/xhr_transport.dart
@@ -44,7 +44,6 @@ class XhrTransportStream implements GrpcTransportStream {
   final StreamController<ByteBuffer> _incomingProcessor = StreamController();
   final StreamController<GrpcMessage> _incomingMessages = StreamController();
   final StreamController<List<int>> _outgoingMessages = StreamController();
-  bool _terminated = false;
 
   @override
   Stream<GrpcMessage> get incomingMessages => _incomingMessages.stream;
@@ -60,7 +59,7 @@ class XhrTransportStream implements GrpcTransportStream {
         .listen((data) => _request.send(data), cancelOnError: true);
 
     _request.onReadyStateChange.listen((data) {
-      if (_incomingMessages.isClosed || _terminated) {
+      if (_incomingProcessor.isClosed) {
         return;
       }
       switch (_request.readyState) {
@@ -81,7 +80,7 @@ class XhrTransportStream implements GrpcTransportStream {
     });
 
     _request.onError.listen((ProgressEvent event) {
-      if (_incomingMessages.isClosed) {
+      if (_incomingProcessor.isClosed) {
         return;
       }
       _onError(GrpcError.unavailable('XhrConnection connection-error'),
@@ -90,7 +89,7 @@ class XhrTransportStream implements GrpcTransportStream {
     });
 
     _request.onProgress.listen((_) {
-      if (_incomingMessages.isClosed) {
+      if (_incomingProcessor.isClosed) {
         return;
       }
       // Use response over responseText as most browsers don't support
@@ -151,7 +150,6 @@ class XhrTransportStream implements GrpcTransportStream {
 
   @override
   Future<void> terminate() async {
-    _terminated = true;
     _close();
     _request.abort();
   }
