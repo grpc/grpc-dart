4e36f42e9030a3cebdff0e0a17fc0f74208aa363
Moritz <mosum@google.com>
2023-08-07 11:35:46 +0200

    Add secure test

---
+++ test/proxy_secure_test.dart
@@ -0,0 +1,73 @@
+@TestOn('vm')
+import 'dart:async';
+import 'dart:io';
+
+import 'package:grpc/grpc.dart';
+import 'package:grpc/src/client/proxy.dart';
+import 'package:test/test.dart';
+
+import 'src/generated/echo.pbgrpc.dart';
+
+void main() {
+  late Server server;
+  late EchoServiceClient fakeClient;
+  late ClientChannel fakeChannel;
+
+  setUp(() async {
+    server = Server.create(services: [FakeEchoService()]);
+    await server.serve(
+      address: 'localhost',
+      port: 8888,
+      security: ServerTlsCredentials(
+        certificate: File('test/data/localhost.crt').readAsBytesSync(),
+        privateKey: File('test/data/localhost.key').readAsBytesSync(),
+      ),
+    );
+    // ignore: prefer_final_locals
+    var proxy = Proxy.direct();
+
+    /// Uncomment this line iff you have proxy running on the port 8080.
+    // proxy = Proxy(host: 'localhost', port: 8080);
+
+    fakeChannel = ClientChannel(
+      'localhost',
+      port: server.port!,
+      options: ChannelOptions(
+        credentials: ChannelCredentials.secure(
+          certificates: File('test/data/localhost.crt').readAsBytesSync(),
+          authority: 'localhost',
+          onBadCertificate: (certificate, host) {
+            print('bad cert for $host issued by ${certificate.issuer}');
+            //Still return true as we allow bad certificates, in case the proxy
+            //wasn't added to our list of trusted authorities.
+            return true;
+          },
+        ),
+        proxy: proxy,
+      ),
+    );
+    fakeClient = EchoServiceClient(fakeChannel);
+  });
+
+  tearDown(() async {
+    await fakeChannel.shutdown();
+    await server.shutdown();
+  });
+
+  test('Sending and receiving over proxy works', () async {
+    final echoRequest = EchoRequest(message: 'blablablubb');
+    final echoResponse = await fakeClient.echo(echoRequest);
+    expect(echoResponse.message, 'blibliblabb');
+  });
+}
+
+class FakeEchoService extends EchoServiceBase {
+  @override
+  Future<EchoResponse> echo(ServiceCall call, EchoRequest request) async =>
+      EchoResponse(message: 'blibliblabb');
+
+  @override
+  Stream<ServerStreamingEchoResponse> serverStreamingEcho(
+          ServiceCall call, ServerStreamingEchoRequest request) =>
+      throw UnimplementedError();
+}
