9538a61c827d8efd3a0c8a5f08c782fb7e713a6e
Ruben Garcia <rubengarciahernandez@gmail.com>
2024-03-25 17:57:00 +0100

    Fix dart analyze issues.

--- test/grpc_compression_flag_test.dart
+++ test/grpc_compression_flag_test.dart
@@ -1,25 +1,24 @@
-import 'package:grpc/src/shared/message.dart';
 import 'package:grpc/src/shared/codec.dart';
-
+import 'package:grpc/src/shared/message.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('GRPC Compression Flag', () {
     test('compression flag 0 with null codec', () {
-      List<int> rawPayload = [1, 2, 3, 4];
-      Codec? codec = null;
+      final rawPayload = <int>[1, 2, 3, 4];
+      final Codec? codec;
       final data = frame(rawPayload, codec);
       expect(data[0], 0);
     });
     test('compression flag 0 with grpc-encoding identity', () {
-      List<int> rawPayload = [1, 2, 3, 4];
-      Codec? codec = IdentityCodec();
+      final rawPayload = <int>[1, 2, 3, 4];
+      final Codec codec = IdentityCodec();
       final data = frame(rawPayload, codec);
       expect(data[0], 0);
     });
     test('compression flag 1 with grpc-encoding gzip', () {
-      List<int> rawPayload = [1, 2, 3, 4];
-      Codec? codec = GzipCodec();
+      final rawPayload = <int>[1, 2, 3, 4];
+      final Codec codec = GzipCodec();
       final data = frame(rawPayload, codec);
       expect(data[0], 1);
     });
