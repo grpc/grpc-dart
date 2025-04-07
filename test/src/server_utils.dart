66feb5d7d093a50e4e76c27267ae0eed215350be
Thomas Hareau <thomas@ozzen.fr>
2025-04-07 11:04:30 +0200

    Remove unused import

--- test/src/server_utils.dart
+++ test/src/server_utils.dart
@@ -17,7 +17,6 @@ import 'dart:async';
 
 import 'package:grpc/grpc.dart';
 import 'package:grpc/src/client/http2_connection.dart';
-import 'package:grpc/src/server/interceptor.dart';
 import 'package:grpc/src/shared/message.dart';
 import 'package:http2/transport.dart';
 import 'package:test/test.dart';
