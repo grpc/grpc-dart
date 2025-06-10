08278ce91b38cbe0da719e9441f1cdbe4fcdd7ea
Devon Carew <devoncarew@gmail.com>
2025-06-10 16:51:09 -0700

    skip coverage check; update file headers

--- lib/src/client/query_parameter.dart
+++ lib/src/client/query_parameter.dart
@@ -1,4 +1,5 @@
-// Copyright 2018 Google Inc. All Rights Reserved.
+// Copyright (c) 2018, the gRPC project authors. Please see the AUTHORS file
+// for details. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
